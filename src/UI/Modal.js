ec14d44bfbd0202f4a6d2133d3552b4360bffb31
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2020-05-24 19:36:05 -0400

    app working

--- src/UI/Modal.js
+++ src/UI/Modal.js
@@ -1,29 +1,39 @@
 export class Modal {
-  constructor(contentId, fallBack) {
-    this.fallBack = fallBack;
+  constructor(contentId, fallbackText) {
+    this.fallbackText = fallbackText;
     this.contentTemplateEl = document.getElementById(contentId);
     this.modalTemplateEl = document.getElementById("modal-template");
   }
-  showModal() {
-    // /**IE check */
+
+  show() {
     if ("content" in document.createElement("template")) {
       const modalElements = document.importNode(
         this.modalTemplateEl.content,
         true
       );
-      const modalElement = document.querySelector(".backdrop");
-      const backDropElement = document.querySelector(".modal");
+      this.modalElement = modalElements.querySelector(".modal");
+      this.backdropElement = modalElements.querySelector(".backdrop");
       const contentElement = document.importNode(
         this.contentTemplateEl.content,
         true
       );
 
-      modalElement.appendChild(contentElement);
-      document.body.insertAdjacentElement("afterbegin", modalElement);
-      document.body.insertAdjacentElement("afterbegin", backDropElement);
+      this.modalElement.appendChild(contentElement);
+
+      document.body.insertAdjacentElement("afterbegin", this.modalElement);
+      document.body.insertAdjacentElement("afterbegin", this.backdropElement);
     } else {
-      alert(this.fallBack);
+      // fallback code
+      alert(this.fallbackText);
     }
   }
-  hideModal() {}
+
+  hide() {
+    if (this.modalElement) {
+      document.body.removeChild(this.modalElement); // this.modalElement.remove()
+      document.body.removeChild(this.backdropElement);
+      this.modalElement = null;
+      this.backdropElement = null;
+    }
+  }
 }
